b6bd52f0a259d65309cda1bf1d0f34b4621cad1b
tommywa-a <tomiwa95@gmail.com>
2025-07-06 18:09:35 +0100

    07 Dashboard Navbar (Add responsive drawer to Dashboard User Button)-2.0

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -98,7 +98,7 @@ export const SignUpView = () => {
 	return (
 		<div className='flex flex-col gap-6'>
 			<Card className='overflow-hidden p-0'>
-				<CardContent className='grid p-0 grid-cols-2'>
+				<CardContent className='grid p-0 md:grid-cols-2'>
 					<Form {...form}>
 						<form
 							onSubmit={form.handleSubmit(onSubmit)}
