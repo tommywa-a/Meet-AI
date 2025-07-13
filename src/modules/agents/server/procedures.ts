2a3bd6b8b38b60ef2fb88def792546e0735c69a7
tommywa-a <tomiwa95@gmail.com>
2025-07-13 17:44:24 +0100

    13: agent filters

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,13 +1,17 @@
 import { z } from "zod";
-import { eq, getTableColumns, sql } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
+
 import { agentsInsertSchema } from "../schemas";
 
 export const agentsRouter = createTRPCRouter({
-  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
     const [existingAgent] = await db
       .select({
         // TODO Change to actual count
@@ -19,16 +23,54 @@ export const agentsRouter = createTRPCRouter({
 
     return existingAgent
   }),
-  getMany: protectedProcedure.query(async () => {
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        page: z.number().default(DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(MIN_PAGE_SIZE)
+          .max(MAX_PAGE_SIZE)
+          .default(DEFAULT_PAGE_SIZE),
+        search: z.string().nullish()
+      })
+  )
+  .query(async ({ ctx, input }) => {
+    const { search, page, pageSize } = input
     const data = await db
       .select({
         // TODO Change to actual count
         ...getTableColumns(agents),
         meetingCount: sql<number>`5`,
       })
-      .from(agents);
+      .from(agents)
+      .where(
+        and(
+          eq(agents.userId, ctx.auth.user.id),
+          search ? ilike(agents.name, `%${search}%`) : undefined,
+        )
+      )
+      .orderBy(desc(agents.createdAt), desc(agents.id))
+      .limit(pageSize)
+      .offset((page - 1) * pageSize)
 
-    return data
+    const [total] = await db
+      .select({count: count() })
+      .from(agents)
+      .where(
+        and(
+          eq(agents.userId, ctx.auth.user.id),
+          search ? ilike(agents.name, `%${search}%`) : undefined,
+        )
+      )
+
+      const totalPages = Math.ceil(total.count / pageSize)
+
+    return {
+      items: data,
+      total: total.count,
+      totalPages,
+    }
   }),
   create: protectedProcedure
     .input(agentsInsertSchema)
--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,14 +1,27 @@
 "use client"
 
-import { PlusIcon } from "lucide-react"
 import { useState } from "react"
+import { PlusIcon, XCircleIcon } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 
 import { NewAgentDialog } from "./new-agent-dialog"
+import { AgentsSearchFilter } from "./agents-search-filter"
+import { useAgentsFilters } from "../../hooks/use-agents.filters"
+import { DEFAULT_PAGE } from "@/constants"
 
 export const AgentsListHeader = () => {
+  const [filters, setFilters] = useAgentsFilters()
   const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+  const isAnyFilterModified = !!filters.search
+
+  const onClearFilters = () => {
+    setFilters({
+      search: "",
+      page: DEFAULT_PAGE,
+    })
+  }
 
   return (
     <>
@@ -20,6 +33,15 @@ export const AgentsListHeader = () => {
             <PlusIcon />
             New Agent
           </Button>
+        </div>
+        <div className="flex items-center gap-x-2 p-1">
+          <AgentsSearchFilter />
+          {isAnyFilterModified && (
+            <Button variant="outline" size="sm" onClick={onClearFilters}>
+              <XCircleIcon />
+              Clear
+            </Button>
+          )}
         </div>
       </div>
     </>
