b45a62931b063b5a29551bd8e8d26a7125e7e6fc
tommywa-a <tomiwa95@gmail.com>
2025-08-03 20:45:54 +0100

    25: completed state (for chapter - completed)

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { format } from "date-fns"
-import humanizeDuration from "humanize-duration"
 import { ColumnDef } from '@tanstack/react-table'
 import { 
 	CircleCheckIcon,
@@ -12,19 +11,11 @@ import {
 	LoaderIcon,
  } from 'lucide-react'
 
- import { cn } from "@/lib/utils"
+ import { cn, formatDuration } from "@/lib/utils"
 import { Badge } from '@/components/ui/badge'
 import { GeneratedAvatar } from '@/components/generated-avatar'
 
 import { MeetingGetMany } from '../../types'
-
-function formatDuration(seconds: number) {
-	return humanizeDuration(seconds * 1000, {
-		largest: 1,
-		round: true,
-		units: ["h", "m", "s"],
-	})
-}
 
 const statusIconMap = {
 	upcoming: ClockArrowUpIcon,
