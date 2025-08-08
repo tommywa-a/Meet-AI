1b8836708dde5626e01a45d91188011bdcab7c80
tommywa-a <tomiwa95@gmail.com>
2025-08-08 23:26:58 +0100

    created premiumRouter

---
+++ src/modules/premium/server/procedures.ts
@@ -0,0 +1,42 @@
+import { eq, count } from "drizzle-orm";
+
+import { db } from "@/db";
+import { agents, meetings } from "@/db/schema";
+import { polarClient } from "@/lib/polar";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/trpc/init";
+
+export const premiumRouter = createTRPCRouter({
+  getFreeUsage: protectedProcedure.query(async ({ ctx }) => {
+    const customer = await polarClient.customers.getStateExternal({
+      externalId: ctx.auth.user.id,
+    })
+
+    const subscription = customer.activeSubscriptions[0]
+
+    if (subscription) {
+      return null
+    }
+
+    const [userMeetings] = await db
+      .select({
+        count: count(meetings.id)
+      })
+      .from(meetings)
+      .where(eq(meetings.userId, ctx.auth.user.id))
+
+    const [userAgents] = await db
+      .select({
+        count: count(agents.id)
+      })
+      .from(agents)
+      .where(eq(agents.userId, ctx.auth.user.id))
+
+      return {
+        meetingCount: userMeetings.count,
+        agentCount:userAgents.count,
+      }
+  })
+})
