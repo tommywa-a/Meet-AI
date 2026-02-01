9c6cbadc7b2603e22d927b845dfcf1b5e93e5ec1
Babatomiwa Aderibigbe <80416515+tommywa-a@users.noreply.github.com>
2026-02-01 23:54:41 +0100

    Update command-select.tsx

--- src/components/command-select.tsx
+++ src/components/command-select.tsx
@@ -3,6 +3,7 @@ import { ReactNode, useState } from "react";
 import { ChevronsUpDownIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
 import {
   CommandEmpty,
